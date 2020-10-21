bc6c4e7d90415cb51d227fc7c3d638516c312f8f
AppKim <jskim2301@yahoo.co.jp>
2020-10-21 18:36:40 +0900

    study_2

---
+++ vue-cli/src/Practice.vue
@@ -0,0 +1,21 @@
+<template>
+
+<!-- HTML -->
+<div>header</div>
+  
+</template>
+
+<script>
+export default {
+    // Javascript - Instance option
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+/* CSS */
+</style>
