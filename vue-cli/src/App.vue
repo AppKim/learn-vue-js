fb895b329b7c1d934f36d2356a17d396979a3911
AppKim <jskim2301@yahoo.co.jp>
2020-10-26 17:44:35 +0900

    study_4(todo_app_create)

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <app-header v-bind:props attribute name='parent component data-name'></app-header> -->
-    <app-header v-bind:propsdata='str' v-on:renew='renewStr'></app-header>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -12,22 +12,22 @@
 //     }
 // });
 
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-    data: function() {
-        return {
-            str: 'Header'
-        }
+  data: function() {
+    return {
+      str: "Header",
+    };
+  },
+  components: {
+    "app-header": AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi";
     },
-    components: {
-        'app-header': AppHeader
-    },
-    methods: {
-      renewStr: function(){
-        this.str = 'hi'
-      }
-    }
+  },
 };
 </script>
 
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ui>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ui>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
+    created() {
+        if (localStorage.length > 0) {
+            for(var i=0; i < localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i))
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
